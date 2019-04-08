5ff9f1a3602b336ac09113419332f686ce02eba7
sebag07 <sag1g15@soton.ac.uk>
2019-04-08 22:49:13 +0100

    Functionality is present, folder is monitored and folders are uploaded to it

--- Client.py
+++ Client.py
@@ -1,210 +1,62 @@
-# import socket
-#
-# def Main():
-#     host = "127.0.0.1"
-#     port = 5002
-#
-#     s = socket.socket()
-#     s.connect((host, port))
-#
-#     filename = input("Filename? ->")
-#     print(filename)
-#     if filename != "q":
-#         s.send(filename.encode())
-#         data = s.recv(1024)
-#         if data[:6] == "EXISTS":
-#             filesize = long(data[6:])
-#             message = input("File Exists. Do you want to download?" + str(filesize) + "Bytes")
-#             if message == 'Y':
-#                 s.send("OK")
-#                 f = open(filename, 'wb')
-#                 data = s.recv(1024)
-#                 totalRecv = len(data)
-#                 f.write(data)
-#                 while totalRecv < filesize:
-#                     data = s.recv(1024)
-#                     totalRecv += len(data)
-#                     f.write(data)
-#                     print("0:,2f".format((totalRecv/float(filesize))*100) + "Done")
-#
-#                 print("Download complete")
-#         else:
-#             print("File doesn't exist")
-#
-#     s.close()
-
-
-# import os, time, sys, socket, shutil
-#
-# host = '127.0.0.1'
-# port = 5552
-#
-# client = socket.socket()
-# client.connect((host, port))
-#
-# path_to_watch = "/home/sebastian/Documents/Scoala/Pexip/ClientFolder"
-# before = dict ([(f, None) for f in os.listdir (path_to_watch)])
-# while 1:
-#   time.sleep (10)
-#   after = dict ([(f, None) for f in os.listdir (path_to_watch)])
-#   added = [f for f in after if not f in before]
-#   removed = [f for f in before if not f in after]
-#   message = ' '
-#   if added:
-#       for i, file in enumerate(added):
-#           print("file" + file)
-#           path_to_file = os.path.join(path_to_watch, file)
-#           print("path_to_file" + path_to_file)
-#           f = open(path_to_file, 'rb')
-#           l = f.read(1024)
-#           print("Added: " + ", ".join(added))
-#           message = "Added: " + ", ".join(added)
-#           client.send(l)
-#           f.close()
-#           # client.send(path_to_watch.encode())
-#   if removed:
-#       print(removed)
-#       print("Removed: " + ", ".join(removed))
-#       message = "Removed: " + ", ".join(removed)
-#       client.send(path_to_watch.encode())
-#   before = after
-
-import socket               # Import socket module
+import socket
 import sys
 import os
 import time
 
-s = socket.socket()         # Create a socket object
-host = socket.gethostname() # Get local machine name
-port = 12345                 # Reserve a port for your service.
-
-path_to_watch = "/home/sebastian/Documents/Scoala/Pexip/ClientFolder"
+# Create a socket object, and connects the client to the host and port of the server.
+s = socket.socket()
+host = socket.gethostname()
+port = 5005
 s.connect((host, port))
-before = dict ([(f, None) for f in os.listdir (path_to_watch)])
 
-while 1:
+# Path of the folder that is monitorized
+path_to_watch = sys.argv[1]
+# Dictionary of the files already present in the monitored folder.
+before = dict ([(f, None) for f in os.listdir (path_to_watch)])
+while True:
+    # Every 5 seconds it looks through the folder to see if files were added or removed.
+    # Added and removed files are both stored in different dictionaries to hold the list of files
+    # each time it looks through the folder. The os library has been used to monitor the folder.
     time.sleep(5)
     after = dict ([(f, None) for f in os.listdir (path_to_watch)])
     added = [f for f in after if not f in before]
     removed = [f for f in before if not f in after]
     if added:
-        message = "added"
-        s.send(message.encode())
         for i, file in enumerate(added):
-            print("file " + file)
+            # Message sent to the server so it knows a file has been added
+            message = "added"
+            s.send(message.encode())
+            print("Added file " + file)
+            # Message sent to let the server know the name of the file that was added
             s.send(file.encode())
+            # Full path of the added file
             path_to_file = os.path.join(path_to_watch, file)
-            print("File path " + path_to_file)
             f = open(path_to_file, 'rb')
+            # Filesize sent to the server so it knows whether to continue sending messages
+            # to fully upload the file. Encoded in utf-8 as files of bigger size had ASCII characters present.
             filesize = str(os.path.getsize(path_to_file))
             s.send(filesize.encode('utf-8'))
-            fileSize = int(filesize)
+            filesize = int(filesize)
             with open(path_to_file, 'rb') as f:
-                l = f.read(1024)
-                totalReceived = len(l)
-                s.send(l)
-                while totalReceived < fileSize:
-                    l = f.read(1024)
-                    totalReceived += len(l)
-                    s.send(l)
+                # Starts sending the file over to the server by continously sending data until
+                # the whole file has been uploaded.
+                data = f.read(1024)
+                totalReceived = len(data)
+                s.send(data)
+                while totalReceived < filesize:
+                    data = f.read(1024)
+                    totalReceived += len(data)
+                    s.send(data)
             print("Done reading")
+            filesize = None
+            time.sleep(0.5)
     if removed:
-        message = "removed"
-        s.send(message.encode())
+        # Message sent to the server so it knows a file has been removed.
+        # Loops through the removed files and sends a message to the server with the removed file.
         for i, file in enumerate(removed):
+            message = "removed"
+            s.send(message.encode())
             print("Removed file " + file)
             s.send(file.encode())
+            time.sleep(0.5)
     before = after
-
-# f = open('text.txt','rb')
-# print('Sending...')
-# l = f.read(1024)
-# while (l):
-#     print('Sending...')
-#     s.send(l)
-#     l = f.read(1024)
-# f.close()
-# print("Done Sending")
-# s.close                     # Close the socket when done
-
-  # if added:
-  #     for i, file in enumerate(added):
-  #         print("file" + file)
-  #         path_to_file = os.path.join(path_to_watch, file)
-  #         print("path_to_file" + path_to_file)
-  #         f = open(path_to_file, 'rb')
-  #         l = f.read(1024)
-  #         print("Added: " + ", ".join(added))
-  #         message = "Added: " + ", ".join(added)
-  #         client.send(l)
-  #         f.close()
-  #         # client.send(path_to_watch.encode())
-  # if removed:
-  #     print(removed)
-  #     print("Removed: " + ", ".join(removed))
-  #     message = "Removed: " + ", ".join(removed)
-  #     client.send(path_to_watch.encode())
-  # before = after
-
-#
-# import socket
-# import threading
-# import os
-# import time
-#
-# import buffer
-#
-# host = '127.0.0.1'
-# port = 2346
-#
-# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# s.connect((host, port))
-#
-# path_to_watch = "/home/sebastian/Documents/Scoala/Pexip/ClientFolder"
-# before = dict([(f, None) for f in os.listdir(path_to_watch)])
-#
-# while 1:
-#     with s:
-#         sbuf = buffer.Buffer(s)
-#
-#         hash_type = 'abc'
-#
-#         time.sleep(10)
-#         after = dict([(f, None) for f in os.listdir(path_to_watch)])
-#         added = [f for f in after if not f in before]
-#         removed = [f for f in before if not f in after]
-#         if added:
-#             for i, file in enumerate(added):
-#                 time.sleep(1)
-#                 print(file)
-#                 path_to_file = os.path.join(path_to_watch, file)
-#                 sbuf.put_utf8(hash_type)
-#                 sbuf.put_utf8(path_to_file)
-#                 print(path_to_file)
-#
-#                 file_size = os.path.getsize(path_to_file)
-#                 print(file_size)
-#                 sbuf.put_utf8(str(file_size))
-#
-#                 with open(path_to_file, 'rb') as f:
-#                     sbuf.put_bytes(f.read())
-#                 print("File sent")
-# with s:
-#     sbuf = buffer.Buffer(s)
-#
-#     hash_type = input('Enter hash type: ')
-#
-#     files = input('Enter file(s) to send: ')
-#     files_to_send = files.split()
-#
-#     for file_name in files_to_send:
-#         print(file_name)
-#         sbuf.put_utf8(hash_type)
-#         sbuf.put_utf8(file_name)
-#
-#         file_size = os.path.getsize(file_name)
-#         sbuf.put_utf8(str(file_size))
-#
-#         with open(file_name, 'rb') as f:
-#             sbuf.put_bytes(f.read())
-#         print('File Sent')
--- Server.py
+++ Server.py
@@ -1,325 +1,64 @@
-import socket               # Import socket module
+import socket
 import os
+import sys
 
-s = socket.socket()         # Create a socket object
-host = socket.gethostname() # Get local machine name
-port = 12345                 # Reserve a port for your service.
-s.bind((host, port))        # Bind to the port
-s.listen(5)                 # Now wait for client connection.
+# Create a socket object, bind the socket to the host of the local machine
+# and assign an avaiable port to it.
+s = socket.socket()
+host = socket.gethostname()
+port = 5005
+s.bind((host, port))
+s.listen(5)
+print("Server started.")
+# Accept connection from client
 client, address = s.accept()
 print("Got connection from" + str(address))
-path_to_copy = "/home/sebastian/Documents/Scoala/Pexip/ServerFolder/"
+# Path of the folder where files will be uploaded
+path_to_copy = sys.argv[1]
 
 while True:
-    message = client.recv(1024).decode()
-    if message == "added":
-        print("Receiving...")
+    print("Receiving")
+    # Decodes message received from client to see if a file was added or if it was removed.
+    message = client.recv(1024)
+    decodedMessage = message.decode()
+    print(decodedMessage)
+    if decodedMessage == "added":
+        # Get the name of the file from client and create path of where the uploaded file
+        # will be created.
         name = client.recv(1024)
         print(name.decode())
         file_name = os.path.join(path_to_copy, name.decode())
-        print(file_name)
+        # Get the filesize of the file to be uploaded from client
         receivedSize = client.recv(1024)
-        print("Decoded Size = " + receivedSize.decode())
-        fileSize = int(receivedSize.decode())
-        print("fileSize = " + str(fileSize))
+        filesize = int(receivedSize.decode())
+        print("File size: " + str(filesize) + " Bytes")
+        # Create the file in the upload folder. If filesize is greater than the bytes received
+        # per message, continue to receive message from client until the entire file has been transferred.
         with open(file_name, 'wb') as f:
-            print("Receiving file...")
-            l = client.recv(1024)
-            totalReceived = len(l)
-            f.write(l)
-            while totalReceived < fileSize:
-                l = client.recv(1024)
-                totalReceived += len(l)
-                f.write(l)
-                print("{0:0.2f}".format((totalReceived/float(fileSize))*100) + "% Done")
+            print("Uploading file...")
+            data = client.recv(1024)
+            totalReceived = len(data)
+            f.write(data)
+            while totalReceived < filesize:
+                data = client.recv(1024)
+                totalReceived += len(data)
+                f.write(data)
+                print("{0:0.2f}".format((totalReceived/float(filesize))*100) + "% Done")
 
-        print("Done receiving")
+        print("File has been uploaded.")
         file_name = None
-        fileSize = None
-    if message == "removed":
-        print("Receiving...")
+        filesize = None
+    if decodedMessage == "removed":
+        # Same as in added, gets the name of the file from client and creates the full path to
+        # upload folder.
         name = client.recv(1024)
         file_name = os.path.join(path_to_copy, name.decode())
-        os.remove(file_name)
-        print(name.decode() + " has been removed")
-
-
-
-# import socket
-# import threading
-# import os
-#
-# def getFile(name, sock):
-#     filename = sock.recv(1024)
-#     print(filename)
-#     print(os.path.isfile(filename))
-#     if os.path.isfile(filename):
-#         sock.send("EXISTS" + str(os.path.getsize(filename.decode())))
-#         userResponse = sock.recv(1024)
-#         if userResponse[:2] == 'OK':
-#             with open(filename, 'rb') as f:
-#                 bytesToSend = f.read(1024)
-#                 sock.send(bytesToSend)
-#                 while bytesToSend != '':
-#                     bytesToSend = f.read(1024)
-#                     sock.send(bytesToSend)
-#
-#     else:
-#         sock.send("ERR")
-#
-#     sock.close()
-#
-# def Main():
-#     host = '127.0.0.1'
-#     port = 5002
-#
-#     # Default to TCP
-#     s = socket.socket()
-#     s.bind((host, port))
-#
-#     s.listen(5)
-#
-#     print("Server started.")
-#     while True:
-#         client, address = s.accept()
-#         print("Client connected ip: " + str(address))
-#         t = threading.Thread(target=getFile, args=("retrThread", client))
-#         t.start()
-#
-#     s.close()
-#
-# if __name__ == "__main__":
-#     Main()
-
-# import socket
-# import os
-#
-# import buffer
-#
-# HOST = ''
-# PORT = 2346
-#
-# # If server and client run in same local directory,
-# # need a separate place to store the uploads.
-# try:
-#     os.mkdir('uploads')
-# except FileExistsError:
-#     pass
-#
-# s = socket.socket()
-# s.bind((HOST, PORT))
-# s.listen(10)
-# print("Waiting for a connection.....")
-#
-# while True:
-#     conn, addr = s.accept()
-#     print("Got a connection from ", addr)
-#     connbuf = buffer.Buffer(conn)
-#
-#     while True:
-#         hash_type = connbuf.get_utf8()
-#         if not hash_type:
-#             break
-#         print('hash type: ', hash_type)
-#
-#         file_name = connbuf.get_utf8()
-#         if not file_name:
-#             break
-#         file_name = os.path.join('ServerFolder',file_name)
-#         print('file name: ', file_name)
-#
-#         file_size = int(connbuf.get_utf8())
-#         print('file size: ', file_size )
-#
-#         with open(file_name, 'wb') as f:
-#             remaining = file_size
-#             while remaining:
-#                 chunk_size = 4096 if remaining >= 4096 else remaining
-#                 chunk = connbuf.get_bytes(chunk_size)
-#                 if not chunk: break
-#                 f.write(chunk)
-#                 remaining -= len(chunk)
-#             if remaining:
-#                 print('File incomplete.  Missing',remaining,'bytes.')
-#             else:
-#                 print('File received successfully.')
-#     print('Connection closed.')
-#     conn.close()
-
-# import socket, shutil
-#
-# server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# host = '127.0.0.1'
-# port = 5552
-#
-# address = (host, port)
-# server.bind(address)
-# server.listen(5)
-# print("Starteg listening on: " + host + " " + str(port))
-# client, addr = server.accept()
-# print("Connection from " + str(addr[0]) + " " + str(addr[1]))
-#
-# path_to_copy =  "/home/sebastian/Documents/Scoala/Pexip/ServerFolder"
-#
-# while True:
-#     data = client.recv(1024)
-#     f = open(data, 'wb')
-#     f.write(data)
-#     f.close()
-#     print("Received " + data.decode() + " from client")
-#     client.send(data)
-
-
-# import socket
-#
-#
-# def server_program():
-#     # get the hostname
-#     host = socket.gethostname()
-#     port = 5001  # initiate port no above 1024
-#
-#     server_socket = socket.socket()  # get instance
-#     # look closely. The bind() function takes tuple as argument
-#     server_socket.bind((host, port))  # bind host address and port together
-#
-#     # configure how many client the server can listen simultaneously
-#     server_socket.listen(2)
-#     conn, address = server_socket.accept()  # accept new connection
-#     print("Connection from: " + str(address))
-#     while True:
-#         # receive data stream. it won't accept data packet greater than 1024 bytes
-#         data = conn.recv(1024).decode()
-#         if not data:
-#             # if data is not received break
-#             break
-#         print("from connected user: " + str(data))
-#         # data = input(' -> ')
-#         # conn.send(data.encode())  # send data to the client
-#
-#     conn.close()  # close the connection
-#
-#
-# if __name__ == '__main__':
-#     server_program()
-
-
-# import socket
-#
-# HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-# PORT = 65431        # Port to listen on (non-privileged ports are > 1023)
-#
-# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-#     s.bind((HOST, PORT))
-#     s.listen()
-#     conn, addr = s.accept()
-#     with conn:
-#         print('Connected by', addr)
-#         while True:
-#             data = conn.recv(1024)
-#             conn.sendall(data)
-
-
-# #!/usr/bin/env python3
-#
-# import selectors
-# import socket
-# sel = selectors.DefaultSelector()
-#
-# host = '127.0.0.1'
-# port = 65431
-#
-# lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# lsock.bind((host, port))
-# lsock.listen()
-# print('listening on', (host, port))
-# lsock.setblocking(False)
-# sel.register(lsock, selectors.EVENT_READ, data=None)
-#
-# while True:
-#     events = sel.select(timeout=None)
-#     for key, mask in events:
-#         if key.data is None:
-#             accept_wrapper(key.fileobj)
-#         else:
-#             service_connection(key, mask)
-#
-# def accept_wrapper(sock):
-#     conn, addr = sock.accept()  # Should be ready to read
-#     print('accepted connection from', addr)
-#     conn.setblocking(False)
-#     data = types.SimpleNamespace(addr=addr, inb=b'', outb=b'')
-#     events = selectors.EVENT_READ | selectors.EVENT_WRITE
-#     sel.register(conn, events, data=data)
-#
-# def service_connection(key, mask):
-#     sock = key.fileobj
-#     data = key.data
-#     if mask & selectors.EVENT_READ:
-#         recv_data = sock.recv(1024)  # Should be ready to read
-#         if recv_data:
-#             data.outb += recv_data
-#         else:
-#             print('closing connection to', data.addr)
-#             sel.unregister(sock)
-#             sock.close()
-#     if mask & selectors.EVENT_WRITE:
-#         if data.outb:
-#             print('echoing', repr(data.outb), 'to', data.addr)
-#             sent = sock.send(data.outb)  # Should be ready to write
-#             data.outb = data.outb[sent:]
-# import socket
-# import sys
-# from _thread import *
-#
-# host = ''
-# port = 5555
-# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#
-# try:
-#     s.bind((host, port))
-# except socket.error as e:
-#     print(str(e))
-#
-# s.listen(5)
-# print('Waiting for a connection.')
-#
-# def client(conn):
-#     conn.send(str.encode('Welcome\n'))
-#
-#     while True:
-#         data = conn.recv(2048)
-#         reply = 'Server output: ' + data.decode('utf-8')
-#         if not data:
-#             break
-#         conn.sendall(str.encode(reply))
-#
-#     conn.close()
-#
-# while True:
-#     conn, addr = s.accept()
-#     print('connected to: ' + addr[0] + ':' + str(addr[1]))
-#
-#     start_new_thread(client, (conn,))
-#
-# import socket
-#
-# serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#
-# serv.bind(('127.0.0.1', 5556))
-# serv.listen(5)
-#
-# while True:
-#     conn, addr = serv.accept()
-#     from_client = ''
-#
-#     while True:
-#         data = conn.recv(2048)
-#         if not data:
-#             break
-#         from_client += data.decode()
-#         print(data)
-#
-#         conn.send('I am Server\n')
-#
-#     conn.close()
-#     print('client disconnected')
+        # Check if file exists in the upload folder
+        if os.path.exists(file_name):
+            # Using the os library we remove the file from there.
+            os.remove(file_name)
+            print(name.decode() + " has been removed")
+            file_name = None
+        else:
+            print("Error. File does not exist.")
+    s.close()
